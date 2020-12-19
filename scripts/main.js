de0f7cf639236383da1611949edb3090ccfb1a69
Jamie Oppel <jimmyoppelpersonal@gmail.com>
2020-12-18 19:23:28 -0800

    Added player factory function, stored names player objects, simplified check for win logic, started function for clearing board

--- scripts/main.js
+++ scripts/main.js
@@ -3,73 +3,93 @@ const gameBoard = (function() {
     //Putting tiles in an array because the project specs demand it
     tiles = []
 
-    //initialize board
+    //grab all the DOM elements
     names = document.getElementById('names')
+    clear = document.getElementById('clear')
     win = document.getElementById('win')
     turn = document.getElementById('turn')
-    turn.innerHTML = 'Player One'
-    for (i=0; i < 9; i++) {
+
+    //function to create player
+    const createPlayer = function(name){
+        return {
+            name: name
+        }
+    }
+    playerOne = createPlayer('Player One')
+    playerTwo = createPlayer('Player Two')
+    turn.innerHTML = playerOne.name
+
+    //Initialize and capture tiles on board
+    const clearBoard = function() {
+        for (i=0; i < 9; i++) {
         let id = parseInt(i + 1)
         tile = document.getElementById(`${id}`)
         tiles.push(tile)
         tile.innerHTML = " ";
+        }
+        turn.innerHTML = playerOne.name
     }
+
+    //Attach function to clear button
+    clear.onclick = clearBoard()
 
     //Attach function to cells for taking turns
     cells = document.querySelectorAll('td')
     for (i = 0; i < cells.length; i++){
         cells[i].onclick = function() {
             if (this.innerHTML === ' ') {
-                if (turn.innerHTML == 'Player One') {
+                if (turn.innerHTML == playerOne.name) {
                     this.innerHTML = 'x'
-                    check_for_draw()
-                    check_for_win('x')
-                    turn.innerHTML = 'Player Two'
+                    if (check_for_win('x')) {
+                        win.innerHTML = `${playerOne.name} wins!`
+                    }
+                    else if (check_for_draw()) {
+                        win.innerHTML = 'It\'s a draw!'
+                    }
+                    turn.innerHTML = playerTwo.name
                 }
                 else {
                     this.innerHTML = 'o'
-                    check_for_draw()
-                    check_for_win('o')
-                    turn.innerHTML = 'Player One'
+                    if (check_for_win('o')) {
+                        win.innerHTML = `${playerTwo.name} wins!`
+                    }
+                    else if (check_for_draw()) {
+                        win.innerHTML = 'It\'s a draw!'
+                    }
+                    turn.innerHTML = playerOne.name
                 }
             }
         }
     }
     //function to check for win
     const check_for_win = function(marker) {
-        if (tiles[0].innerHTML == marker && tiles[1].innerHTML == marker && tiles[2].innerHTML == marker) {
-            win.innerHTML = turn.innerHTML + " wins!"
+        if ((tiles[0].innerHTML == marker && tiles[1].innerHTML == marker && tiles[2].innerHTML == marker) ||
+            (tiles[3].innerHTML == marker && tiles[4].innerHTML == marker && tiles[5].innerHTML == marker) || 
+            (tiles[6].innerHTML == marker && tiles[7].innerHTML == marker && tiles[8].innerHTML == marker) ||
+            (tiles[0].innerHTML == marker && tiles[3].innerHTML == marker && tiles[6].innerHTML == marker) ||
+            (tiles[1].innerHTML == marker && tiles[4].innerHTML == marker && tiles[7].innerHTML == marker) ||
+            (tiles[2].innerHTML == marker && tiles[5].innerHTML == marker && tiles[8].innerHTML == marker) ||
+            (tiles[0].innerHTML == marker && tiles[4].innerHTML == marker && tiles[8].innerHTML == marker) ||
+            (tiles[2].innerHTML == marker && tiles[4].innerHTML == marker && tiles[6].innerHTML == marker)) {
+            return true
         }
-        else if (tiles[3].innerHTML == marker && tiles[4].innerHTML == marker && tiles[5].innerHTML == marker) {
-            win.innerHTML = turn.innerHTML + " wins!"
-        }
-        else if (tiles[6].innerHTML == marker && tiles[7].innerHTML == marker && tiles[8].innerHTML == marker) {
-            win.innerHTML = turn.innerHTML + " wins!"
-        }
-        else if (tiles[0].innerHTML == marker && tiles[3].innerHTML == marker && tiles[6].innerHTML == marker) {
-            win.innerHTML = turn.innerHTML + " wins!"
-        }
-        else if (tiles[1].innerHTML == marker && tiles[4].innerHTML == marker && tiles[7].innerHTML == marker) {
-            win.innerHTML = turn.innerHTML + " wins!"
-        }
-        else if (tiles[2].innerHTML == marker && tiles[5].innerHTML == marker && tiles[8].innerHTML == marker) {
-            win.innerHTML = turn.innerHTML + " wins!"
-        }
-        else if (tiles[0].innerHTML == marker && tiles[4].innerHTML == marker && tiles[8].innerHTML == marker) {
-            win.innerHTML = turn.innerHTML + " wins!"
-        }
-        else if (tiles[2].innerHTML == marker && tiles[4].innerHTML == marker && tiles[6].innerHTML == marker) {
-            win.innerHTML = turn.innerHTML + " wins!"
-        }
+        return false
     }
 
+    //function to check for draw
     const check_for_draw = function() {
         draw = true
         for (i = 0; i < tiles.length; i++) {
-            if (tiles[i].innerHTML == ' ') {draw = false}
+            if (tiles[i].innerHTML == ' ') {return false}
         }
-        if (draw == true) {
-            win.innerHTML = "It's a draw!"
-        }
+        return true
+    }
+
+    //function for making players
+
+    return {
+        clearBoard: clearBoard
     }
 })();
+
+gameBoard.clearBoard()
